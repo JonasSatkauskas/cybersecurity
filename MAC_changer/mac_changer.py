193217069a5f2521f5fda8e227f3d0fe3cdca5fb
Satkus <satkusmedia@gmail.com>
2021-04-17 17:52:51 +0300

    Tai yra pirmas normalus pushas

---
+++ MAC_changer/mac_changer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Command to see permanent MAC address:
+# macchanger -s <DEVICENAME>
+
+import subprocess
+import optparse
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    print("[+] Success \n")
+    subprocess.call(["macchanger", "-s", interface])
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+change_mac(options.interface, options.new_mac)
+
+
+
