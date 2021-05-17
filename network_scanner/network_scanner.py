cd56be2228199971b34483467adeadd2d6e03e21
satkus <satkusmedia@gmail.com>
2021-05-17 18:16:51 -0400

    Parse the response

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,4 +1,5 @@
 # terminal: route -n    (TO KNOW WHAT IS MY GATEWAY)
+# .summary() is a part of scapy which shows more information
 import scapy.all as scapy
 
 
@@ -6,9 +7,12 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)      #TIMEOUT IS A MUST, OTHEWISE U'LL BE STUCK
-    print(unanswered.summary())
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]    # TIMEOUT IS A MUST
+
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("---------------------------------------------------------")
 
 
 scan("10.0.2.1/24")
-
