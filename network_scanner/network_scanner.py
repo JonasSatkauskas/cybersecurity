44aab3068059db3c0d8d6ff07e5d6743e4a99660
satkus <satkusmedia@gmail.com>
2021-05-17 17:25:26 -0400

    Sending and receiving packets

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -4,11 +4,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    arp_request_broadcast.show()
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)      #TIMEOUT IS A MUST, OTHEWISE U'LL BE STUCK
+    print(unanswered.summary())
 
 
 scan("10.0.2.1/24")
